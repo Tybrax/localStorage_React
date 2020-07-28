f8a2a44e0489ef23e278cbd716a0c20352ff8f6b
Bastien <bastien.ratat@gmail.com>
2020-07-28 19:12:51 +0200

    implement a simple React component using the local storage logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState(
+      localStorage.getItem('localStorageValue') || ''
+    );
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("localStorageValue", value);
+  }, [value]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={value} type="text" onChange={onChange} />
+        <p>{value}</p>
       </header>
     </div>
   );
